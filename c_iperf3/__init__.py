f1b45fe79bea20a39a852b505ed44e046fc833fc
VirtualTam <virtualtam@flibidi.net>
2017-05-10 16:05:28 +0200

    packaging: update metadata
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

---
+++ c_iperf3/__init__.py
@@ -0,0 +1,5 @@
+"""iperf3-cython-wrapper"""
+__title__ = 'iperf3-cython-wrapper'
+__brief__ = 'IPerf3 C API wrapper'
+__version__ = '0.1.0'
+__author__ = 'VirtualTam'
--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+"""Setup script for iperf3-cython-wrapper"""
 from setuptools import find_packages, setup, Extension
-from Cython.Build import cythonize
+import codecs
 import os
+import re
+
+from Cython.Build import cythonize
 
 EXTENSIONS = [
     Extension(
@@ -11,12 +16,32 @@ EXTENSIONS = [
 ]
 
 
+def get_long_description():
+    """Reads the main README.rst to get the program's long description"""
+    with codecs.open('README.rst', 'r', 'utf-8') as f_readme:
+        return f_readme.read()
+
+
+def get_package_metadata(attribute):
+    """Reads metadata from the main package's __init__"""
+    with open(os.path.join('c_iperf3', '__init__.py'), 'r') as f_init:
+        return re.search(
+            r'^__{attr}__\s*=\s*[\'"]([^\'"]*)[\'"]'.format(attr=attribute),
+            f_init.read(), re.MULTILINE
+        ).group(1)
+
+
 setup(
-    name='iperf3-cython-wrapper',
-    version='1.0.0',
-    description="IPerf3 Cython wrapper",
-    long_description="IPerf3 Cython wrapper",
-    ext_modules=cythonize(EXTENSIONS),
+    name=get_package_metadata('title'),
+    version=get_package_metadata('version'),
+    description=get_package_metadata('brief'),
+    long_description=get_long_description(),
+    author=get_package_metadata('author'),
+    maintainer='VirtualTam',
+    maintainer_email='virtualtam@flibidi.net',
+    license='MIT',
+    url='https://github.com/virtualtam/iperf3-cython-wrapper',
+    keywords='network throughput iperf3',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -33,5 +58,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Topic :: System :: Networking',
         'Topic :: Utilities',
-    ]
+    ],
+    ext_modules=cythonize(EXTENSIONS)
 )
