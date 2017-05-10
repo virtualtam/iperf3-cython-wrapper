af9e9f3e2b8e7fe9bbceed4bc3a583d3db447f59
VirtualTam <virtualtam@flibidi.net>
2017-05-10 15:50:56 +0200

    packaging: add LICENSE, update classifiers, add py35 Tox env
    Signed-off-by: VirtualTam <virtualtam@flibidi.net>

--- setup.py
+++ setup.py
@@ -16,5 +16,22 @@ setup(
     version='1.0.0',
     description="IPerf3 Cython wrapper",
     long_description="IPerf3 Cython wrapper",
-    ext_modules=cythonize(EXTENSIONS)
+    ext_modules=cythonize(EXTENSIONS),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Cython',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities',
+    ]
 )
